16079940ff1ff3004d71d01ab51ca6b8ae97e399
jeanboy <eesidoro44@gmail.com>
2023-05-29 16:20:39 +0900

    fruit class done

--- src/app.py
+++ src/app.py
@@ -122,7 +122,7 @@ cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
-apple = pygame.image.load("resources/다운로드.png").convert_alpha()
+apple = pygame.image.load("resources/apple.png").convert_alpha()
 
 main_game = Main()
 
